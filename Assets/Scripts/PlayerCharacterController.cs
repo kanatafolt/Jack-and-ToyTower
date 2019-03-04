4c66388f0074b0ac743300904e1244b93a852acf
kanatafolt <kanata.folt@gmail.com>
2019-03-05 07:07:28 +0900

    Implemented rotation spring
    回転ばねの挙動を実装した。

--- Assets/Scripts/PlayerCharacterController.cs
+++ Assets/Scripts/PlayerCharacterController.cs
@@ -9,85 +9,98 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerCharacterController : MonoBehaviour
 {
-    const float MAX_JUMP_POWER = 0.6f;
-    const float MIN_SPRING_SCALE = 0.1f;
+    const float MAX_JUMP_CHARGE = 0.6f;     //ジャンプの最大溜め時間及びジャンプ力係数
+    const float MIN_SPRING_SCALE = 0.1f;    //ばねの最小縮み長さ
+    const float MOVE_FREGQUENCY = 0.2f;     //移動発生周期
 
-    [SerializeField] GameObject springObj;
+    [SerializeField] GameObject springObj, coverObj;
     private Rigidbody rb;
 
-    private float jumpPower = 0.0f;
-    private float initialSpringScale = 1.0f;
-    [SerializeField] float moveFrequency = 0.1f;
-    private float moveCycle = 0.0f;
+    private float initialSpringScale;       //ジャンプキーを押下した瞬間のバネの長さを一時保存する
+    private float jumpCharge, moveCharge;
+    private Vector3 moveDir;
 
-    private void Start()
+    private void Reset()
+    {
+        springObj = GameObject.Find("SpringRig");
+        coverObj = GameObject.Find("CoverRig");
+    }
+
+    void Start()
     {
         rb = GetComponent<Rigidbody>();
+        moveDir = transform.forward;
     }
 
     void Update()
     {
+        //移動周期を計算
+        if (moveCharge < MOVE_FREGQUENCY) moveCharge += Time.deltaTime;
+        
         if (Input.GetButton("Vertical") || Input.GetButton("Horizontal"))
         {
-            moveCycle += Time.deltaTime;
-            if(moveCycle >= moveFrequency)
+            //移動キーを押している間：周期的に小ジャンプ移動
+            if(moveCharge >= MOVE_FREGQUENCY)
             {
+                moveCharge -= MOVE_FREGQUENCY;
 
-                moveCycle -= moveFrequency;
+                //一回の小ジャンプ移動が発生
+                moveDir = (Vector3.forward * Input.GetAxis("Vertical") + Vector3.right * Input.GetAxis("Horizontal")).normalized;
+                rb.velocity += moveDir * 2.0f;
+                rb.velocity += transform.TransformDirection(Vector3.up * 1.0f);
+                //springObj.transform.localScale = new Vector3(1.0f, springObj.transform.localScale.y - 0.1f, 1.0f);
+                //coverObj.transform.Rotate(Vector3.right * 15.0f);
+                springObj.GetComponent<SpringSimulation>().SetImpulse(-0.01f, 0.1f);
+                coverObj.GetComponent<SpringSimulation>().SetImpulse(-1.0f, 0.1f);
             }
         }
 
-        if (Input.GetButtonUp("Vertical") || Input.GetButtonUp("Horizontal"))
+        if ((Input.GetButtonUp("Vertical") && !Input.GetButton("Horizontal")) || (Input.GetButtonUp("Horizontal") && !Input.GetButton("Vertical")) || (Input.GetButtonUp("Vertical") && Input.GetButtonUp("Horizontal")))
         {
-            moveCycle = 0.0f;
-        }
-
-        
-
-        if (Input.GetAxis("Vertical") > 0)
-        {
-            //前へ移動
-        }
-
-        if (Input.GetAxis("Vertical") < 0)
-        {
-            //後ろへ移動
-        }
-
-        if (Input.GetAxis("Horizontal") > 0)
-        {
-            //右へ移動
-        }
-
-        if (Input.GetAxis("Horizontal") < 0)
-        {
-            //左へ移動
+            //移動キーを完全に離したとき：キャラクター向きの自動回転を中断する
+            moveDir = transform.forward;
         }
 
         if (Input.GetButtonDown("Jump"))
         {
+            //ジャンプキーを押したとき：ばねの挙動を一時的に止める
             springObj.GetComponent<SpringSimulation>().enableSpring = false;
             initialSpringScale = springObj.transform.localScale.y;
         }
 
         if (Input.GetButton("Jump"))
         {
-            //体を縮める
-            jumpPower += Time.deltaTime;
-            if (jumpPower > MAX_JUMP_POWER) jumpPower = MAX_JUMP_POWER;
+            //ジャンプキーを押している間：押す長さに応じて体を縮める(jumpChargeが溜まる)
+            jumpCharge += Time.deltaTime;
+            if (jumpCharge > MAX_JUMP_CHARGE) jumpCharge = MAX_JUMP_CHARGE;
 
             float shrinkLen = initialSpringScale - MIN_SPRING_SCALE;
-            shrinkLen *= jumpPower / MAX_JUMP_POWER;
+            shrinkLen *= jumpCharge / MAX_JUMP_CHARGE;
             springObj.transform.localScale = new Vector3(1.0f, initialSpringScale - shrinkLen, 1.0f);
-
         }
 
         if (Input.GetButtonUp("Jump"))
         {
-            //ジャンプ
+            //ジャンプキーを離したとき：jumpChargeの値に応じてジャンプする
             springObj.GetComponent<SpringSimulation>().enableSpring = true;
-            rb.velocity += transform.TransformDirection(Vector3.up * (jumpPower * 8.0f));
-            jumpPower = 0.0f;
+            rb.velocity += transform.TransformDirection(Vector3.up * (jumpCharge * 8.0f));
+            jumpCharge = 0.0f;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //キャラクターが進行方向をゆっくりと向く
+        if (Vector3.Angle(transform.forward, moveDir) >= 0.1f)
+        {
+            float rotDiff = Vector3.Angle(transform.forward, moveDir);
+            if (rotDiff >= 5.0f) rotDiff = 5.0f;
+            else rotDiff *= 0.1f;
+            if (transform.InverseTransformDirection(moveDir).x < 0) rotDiff *= -1;
+            transform.Rotate(Vector3.up * rotDiff);
+        } else
+        {
+            if(moveDir != Vector3.zero) transform.forward = moveDir;
         }
     }
 }
--- Assets/Scripts/SpringSimulation.cs
+++ Assets/Scripts/SpringSimulation.cs
@@ -1,5 +1,6 @@
 ﻿//
-//スケール変化でばねの挙動を表現するスクリプト
+//スケール変化や位置・回転ででばねの挙動を表現するスクリプト
+//SpringTransform.position(座標ばね)は未実装
 //
 
 using System.Collections;
@@ -8,42 +9,99 @@ using UnityEngine;
 
 public class SpringSimulation : MonoBehaviour
 {
-    enum ScaleAxis { x, y, z }
-    [SerializeField] ScaleAxis scaleAxis = ScaleAxis.y;
+    enum SpringAxis { x, y, z }                             //バネの挙動を取る軸
+    [SerializeField] SpringAxis springAxis = SpringAxis.y;
 
-    private Vector3 origin;
-    private float acceleration = 0.0f;
+    enum SpringTransform { position, rotation, scale }      //バネが影響するトランスフォーム要素
+    [SerializeField] SpringTransform springTransform = SpringTransform.scale;
+
+    [SerializeField] Transform coordinateParent;
+    private Vector3 originPosition, originScale;
+    private Vector3 originAngleX, originAngleY, originAngleZ;
     private float velocity = 0.0f;
 
     public bool enableSpring = true;
     [SerializeField] float accRate = 0.2f;
     [SerializeField] float velReduceRate = 0.9f;
 
+    private float impulseTime, impulsePower;
+
     void Start()
     {
-        origin = transform.localScale;
+        if (!coordinateParent) coordinateParent = transform;
+
+        originPosition = transform.localPosition;
+        originScale = transform.localScale;
+        originAngleX = coordinateParent.InverseTransformDirection(transform.right);
+        originAngleY = coordinateParent.InverseTransformDirection(transform.up);
+        originAngleZ = coordinateParent.InverseTransformDirection(transform.forward);
     }
 
     void FixedUpdate()
     {
+        //インパルス処理
+        if (impulseTime > 0.0f) impulseTime -= Time.deltaTime;
+        else impulsePower = 0.0f;
+
         if (enableSpring)
         {
-            float diff = 0.0f;
-            Vector3 expansion = Vector3.zero;
+            //スケールばねの場合
+            if (springTransform == SpringTransform.scale)
+            {
+                float diff = 0.0f;
+                Vector3 expansion = Vector3.zero;
 
-            if (scaleAxis == ScaleAxis.x) diff = origin.x - transform.localScale.x;
-            if (scaleAxis == ScaleAxis.y) diff = origin.y - transform.localScale.y;
-            if (scaleAxis == ScaleAxis.z) diff = origin.z - transform.localScale.z;
+                if (springAxis == SpringAxis.x) diff = originScale.x - transform.localScale.x;
+                if (springAxis == SpringAxis.y) diff = originScale.y - transform.localScale.y;
+                if (springAxis == SpringAxis.z) diff = originScale.z - transform.localScale.z;
 
-            acceleration = diff * accRate;
-            velocity += acceleration;
-            velocity *= velReduceRate;
+                float acc = diff * accRate;
+                velocity += acc;
+                velocity *= velReduceRate;
 
-            if (scaleAxis == ScaleAxis.x) expansion = Vector3.right * velocity;
-            if (scaleAxis == ScaleAxis.y) expansion = Vector3.up * velocity;
-            if (scaleAxis == ScaleAxis.z) expansion = Vector3.forward * velocity;
+                velocity += impulsePower;
 
-            transform.localScale += expansion;
+                if (springAxis == SpringAxis.x) expansion = Vector3.right * velocity;
+                if (springAxis == SpringAxis.y) expansion = Vector3.up * velocity;
+                if (springAxis == SpringAxis.z) expansion = Vector3.forward * velocity;
+
+                transform.localScale += expansion;
+            }
+
+            //回転ばねの場合
+            if (springTransform == SpringTransform.rotation)
+            {
+                float diff = 0.0f;
+                Vector3 rot = Vector3.zero;
+
+                if (springAxis == SpringAxis.x) diff = Vector3.Angle(coordinateParent.InverseTransformDirection(transform.up), originAngleY);
+                if (springAxis == SpringAxis.y) diff = Vector3.Angle(coordinateParent.InverseTransformDirection(transform.forward), originAngleZ);
+                if (springAxis == SpringAxis.z) diff = Vector3.Angle(coordinateParent.InverseTransformDirection(transform.right), originAngleX);
+
+                float acc = diff * accRate;
+
+                if (springAxis == SpringAxis.x && transform.InverseTransformDirection(coordinateParent.TransformDirection(originAngleY)).z < 0) acc *= -1;
+                if (springAxis == SpringAxis.y && transform.InverseTransformDirection(coordinateParent.TransformDirection(originAngleZ)).x < 0) acc *= -1;
+                if (springAxis == SpringAxis.z && transform.InverseTransformDirection(coordinateParent.TransformDirection(originAngleX)).y < 0) acc *= -1;
+
+                velocity += acc;
+                velocity *= velReduceRate;
+
+                velocity += impulsePower;
+
+                if (springAxis == SpringAxis.x) rot = Vector3.right * velocity;
+                if (springAxis == SpringAxis.y) rot = Vector3.up * velocity;
+                if (springAxis == SpringAxis.z) rot = Vector3.forward * velocity;
+
+                transform.Rotate(rot);
+            }
         }
+    }
+
+    //バネに撃力を与える。powはかかる力の大きさ、tはインパルス時間を設定
+    public void SetImpulse (float pow, float t)
+    {
+        impulsePower = pow;
+        impulseTime = t;
     }
 }
